fa72f6386c8ca0bf662f2ddf51aa8eb6b6391573
Carlos <carlossouza_94@hotmail.com>
2021-05-02 01:54:30 -0300

    Added

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/html");
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
